11a7a2a6446d4a845e0c4bcd48abd0d4a0478c7f
Filipe Dias <filipediascontato@gmail.com>
2021-11-16 20:03:26 -0300

    Adds parameter validations

--- MoviesAPI/Models/Movie.cs
+++ MoviesAPI/Models/Movie.cs
@@ -1,13 +1,19 @@
-﻿namespace MoviesAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAPI.Models
 {
     public class Movie
     {
+        [Required(ErrorMessage = "The title field is required.")]
         public string Title { get; set; }
 
+        [Required]
         public string Director { get; set; }
 
+        [StringLength (100, ErrorMessage = "The Genre field must be at most 100 characters.")]
         public string Genre { get; set; }
 
+        [Range(1, 600, ErrorMessage = "The Duration field must be at least 1 minute and at most 600 minutes")]
         public int Duration {  get; set; }
     }
 }
