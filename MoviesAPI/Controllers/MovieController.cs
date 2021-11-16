73b38bfa462b643b6bc747413c9c6a804091b4ea
Filipe Dias <filipediascontato@gmail.com>
2021-11-16 19:53:57 -0300

    Initial commit

---
+++ MoviesAPI/Controllers/MovieController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Models;
+
+namespace MoviesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        private static List<Movie> movies = new List<Movie>();
+
+        [HttpPost(Name = "PostMovie")]
+        public void AddMovie([FromBody] Movie movie)
+        {
+            movies.Add(movie);
+        }
+    }
+}
---
+++ MoviesAPI/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace MoviesAPI.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+
+        public string Director { get; set; }
+
+        public string Genre { get; set; }
+
+        public int Duration {  get; set; }
+    }
+}
