dadd4cd424be9323e4cb78d8054732c5fd36d656
Filipe Dias <filipediascontato@gmail.com>
2021-11-17 02:12:43 -0300

    Adds the connection string

--- MoviesAPI/Program.cs
+++ MoviesAPI/Program.cs
@@ -1,7 +1,13 @@
+using MoviesAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("default");
+builder.Services.AddDbContext<MovieContext>(options => options.UseNpgsql(connectionString));
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
