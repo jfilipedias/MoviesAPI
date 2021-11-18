38fb63c6c5c52e3f93722cb80bc92be82f8e7920
Filipe Dias <filipediascontato@gmail.com>
2021-11-18 18:15:14 -0300

    Creates the Theater model

---
+++ MoviesAPI/Models/Theater.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAPI.Models
+{
+    public class Theater
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The name field are required")]
+        public string Name { get; set; }
+    }
+}
