d1c58029575082a6d967fa92fd4b4715bf67f553
Filipe Dias <filipediascontato@gmail.com>
2021-11-18 18:15:33 -0300

    Start to implement the Theater controller

---
+++ MoviesAPI/Controllers/TheaterController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Data;
+using MoviesAPI.Models;
+
+namespace MoviesAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TheaterController : ControllerBase
+    {
+        private AppDbContext _context;
+        private IMapper _mapper;
+
+        public TheaterController(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpPost(Name = "CreateTheater")]
+        public IActionResult AddTheater([FromBody] Theater cinemaDto)
+        {
+
+        }
+    }
+}
