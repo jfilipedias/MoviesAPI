94586229722fda318243564a3a4d2e7ba9717227
Filipe Dias <filipediascontato@gmail.com>
2021-11-18 19:34:28 -0300

    Creates the TheaterProfile

---
+++ MoviesAPI/Profiles/TheaterProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using MoviesAPI.Data.Dtos.Theater;
+using MoviesAPI.Models;
+
+namespace MoviesAPI.Profiles
+{
+    public class TheaterProfile : Profile
+    {
+        public TheaterProfile()
+        {
+            CreateMap<CreateTheaterDto, Theater>();
+            CreateMap<Theater, ReadTheaterDto>();
+            CreateMap<UpdateTheaterDto, Theater>();
+        }
+    }
+}
