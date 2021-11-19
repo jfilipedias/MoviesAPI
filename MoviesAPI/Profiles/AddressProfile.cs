da345f5d2559291606fd8544287b4b033ee8ad6a
Filipe Dias <filipediascontato@gmail.com>
2021-11-19 14:04:52 -0300

    Creates tje Address profile to automapper

---
+++ MoviesAPI/Profiles/AddressProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using MoviesAPI.Data.Dtos.Address;
+using MoviesAPI.Models;
+
+namespace MoviesAPI.Profiles
+{
+    public class AddressProfile : Profile
+    {
+        public AddressProfile()
+        {
+            CreateMap<CreateAddressDto, Address>();
+            CreateMap<Address, ReadAddressDto>();
+            CreateMap<UpdateAddressDto, Address>();
+        }
+    }
+}
