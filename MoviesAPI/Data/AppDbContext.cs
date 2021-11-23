f6729430bb41c54cb7cb48620f0ac70299d00155
Filipe Dias <filipediascontato@gmail.com>
2021-11-22 23:10:03 -0300

    Adds DbSet Manager to the AppDbContext

--- MoviesAPI/Data/AppDbContext.cs
+++ MoviesAPI/Data/AppDbContext.cs
@@ -6,9 +6,8 @@ namespace MoviesAPI.Data
     public class AppDbContext : DbContext
     {
         public DbSet<Address> Addresses { get; set; }
-
+        public DbSet<Manager> Managers { get; set; }
         public DbSet<Movie> Movies { get; set; }
-
         public DbSet<Theater> Theaters { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
