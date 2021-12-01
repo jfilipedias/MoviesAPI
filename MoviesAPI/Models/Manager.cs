a35740ad4516298df41f0276c6862fdd50114dde
Filipe Dias <filipediascontato@gmail.com>
2021-11-30 23:23:36 -0300

    Fix eol

--- MoviesAPI/Models/Manager.cs
+++ MoviesAPI/Models/Manager.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace MoviesAPI.Models
 {
@@ -13,6 +14,7 @@ namespace MoviesAPI.Models
         [Required]
         public string Name { get; set; }
 
+        [JsonIgnore]
         public virtual List<Theater> Theaters { get; set; }
     }
 }
