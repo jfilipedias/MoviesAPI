4d3b701b56900fefc020e554a16b0f00f7170a57
Filipe Dias <filipediascontato@gmail.com>
2021-12-06 20:53:52 -0300

    Fix /movie/{id} GET route response type

--- MoviesAPI/Controllers/MovieController.cs
+++ MoviesAPI/Controllers/MovieController.cs
@@ -45,7 +45,7 @@ namespace MoviesAPI.Controllers
 
         [HttpGet("{id}", Name = "GetMovieById")]
         [SwaggerOperation(Summary = "Lists a movie by id", Description = "Lists a movie by id")]
-        [SwaggerResponse(200, "The given movie has been listed", typeof(ReadAddressDto))]
+        [SwaggerResponse(200, "The given movie has been listed", typeof(ReadMovieDto))]
         [SwaggerResponse(404, "The given movie was not found")]
         public IActionResult GetMovieById(int id)
         {
