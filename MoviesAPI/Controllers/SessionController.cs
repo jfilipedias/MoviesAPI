dcfc7d92565bff5bc09ebec0f070d33d77790f4f
Filipe Dias <filipediascontato@gmail.com>
2021-12-08 08:45:25 -0300

    Refacts the SessionController

--- MoviesAPI/Controllers/SessionController.cs
+++ MoviesAPI/Controllers/SessionController.cs
@@ -1,8 +1,6 @@
-﻿using AutoMapper;
-using Microsoft.AspNetCore.Mvc;
-using MoviesAPI.Data;
+﻿using Microsoft.AspNetCore.Mvc;
 using MoviesAPI.Data.Dtos;
-using MoviesAPI.Models;
+using MoviesAPI.Services;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace MoviesAPI.Controllers
@@ -12,79 +10,69 @@ namespace MoviesAPI.Controllers
     [SwaggerTag("Create, read, update and delete sessions")]
     public class SessionController : ControllerBase
     {
-        private AppDbContext _context;
-        private IMapper _mapper;
+        private SessionService _sessionService;
 
-        public SessionController(AppDbContext context, IMapper mapper)
+        public SessionController(SessionService sessionService)
         {
-            _context = context;
-            _mapper = mapper;
+            _sessionService = sessionService;
         }
 
         [HttpPost(Name = "CreateSession")]
-        [SwaggerOperation(Summary = "Creates a new session", Description = "Adds a new sessionto the database")]
-        [SwaggerResponse(201, "The session was created", typeof(Session))]
-        [SwaggerResponse(400, "The session data is invalid")]
+        [SwaggerOperation(Summary = "Creates a new session.", Description = "Creates a new session.")]
+        [SwaggerResponse(201, "The session was created.", typeof(ReadSessionDto))]
+        [SwaggerResponse(400, "The session data is invalid.")]
         public IActionResult CreateSession([FromBody] CreateSessionDto createSessionDto)
         {
-            var session = _mapper.Map<Session>(createSessionDto);
-
-            _context.Sessions.Add(session);
-            _context.SaveChanges();
-
-            return CreatedAtAction(nameof(GetSessionById), new { Id = session.Id }, session);
+            var readSessionDto = _sessionService.CreateSession(createSessionDto);
+            
+            return CreatedAtAction(nameof(GetSessionById), new { Id = readSessionDto.Id }, readSessionDto);
         }
 
-        [HttpGet(Name = "GetAllSessions")]
-        [SwaggerOperation(Summary = "Lists all sessions", Description = "Return the sessions in the database")]
-        [SwaggerResponse(200, "All existing sessions have been listed", typeof(List<Session>))]
-        public IActionResult GetAllSessions()
+        [HttpGet(Name = "GetSessions")]
+        [SwaggerOperation(Summary = "Gets a sessions list.", Description = "Gets a sessions list.")]
+        [SwaggerResponse(200, "All existing sessions have been listed.", typeof(List<ReadSessionDto>))]
+        public IActionResult GetSessions()
         {
-            return Ok(_context.Sessions);
+            var readSessionDtos = _sessionService.GetSession();
+            
+            return Ok(readSessionDtos);
         }
 
         [HttpGet("{id}", Name = "GetSessionById")]
-        [SwaggerOperation(Summary = "Lists a session by id", Description = "Lists a session by id")]
-        [SwaggerResponse(200, "The given session has been listed", typeof(ReadSessionDto))]
-        [SwaggerResponse(400, "The given session was not found")]
+        [SwaggerOperation(Summary = "Gets a session by id.", Description = "Gets a session by id.")]
+        [SwaggerResponse(200, "The given session has been listed.", typeof(ReadSessionDto))]
+        [SwaggerResponse(400, "The given session was not found.")]
         public IActionResult GetSessionById(int id)
         {
-            var session = _context.Sessions.FirstOrDefault(session => session.Id == id);
+            ReadSessionDto? readSessionDto = _sessionService.GetSessionById(id);
 
-            if (session == null) return NotFound();
+            if (readSessionDto == null) return NotFound();
 
-            var readTheaterDto = _mapper.Map<ReadSessionDto>(session);
-            return Ok(readTheaterDto);
+            return Ok(readSessionDto);
         }
 
         [HttpPut("{id}", Name = "UpdateSession")]
-        [SwaggerOperation(Summary = "Updates a session by id", Description = "Updates a session by id")]
-        [SwaggerResponse(204, "The given session has been updated")]
-        [SwaggerResponse(404, "The given session was not found")]
+        [SwaggerOperation(Summary = "Updates a session by id.", Description = "Updates a session by id.")]
+        [SwaggerResponse(204, "The given session has been updated.")]
+        [SwaggerResponse(404, "The given session was not found.")]
         public IActionResult UpdateSession(int id, [FromBody] UpdateSessionDto updateSessionDto)
         {
-            var session = _context.Sessions.FirstOrDefault(session => session.Id == id);
+            var result = _sessionService.UpdateSession(id, updateSessionDto);
 
-            if (session == null) return NotFound();
-
-            _mapper.Map(updateSessionDto, session);
-            _context.SaveChanges();
+            if (result.IsFailed) return NotFound();
 
             return NoContent();
         }
 
         [HttpDelete("{id}", Name = "DeleteSessions")]
-        [SwaggerOperation(Summary = "Deletes a session by id", Description = "Deletes a session by id")]
-        [SwaggerResponse(204, "The given session has been deleted")]
-        [SwaggerResponse(404, "The given session was not found")]
+        [SwaggerOperation(Summary = "Deletes a session by id.", Description = "Deletes a session by id.")]
+        [SwaggerResponse(204, "The given session has been deleted.")]
+        [SwaggerResponse(404, "The given session was not found.")]
         public IActionResult DeleteSession(int id)
         {
-            var session = _context.Sessions.FirstOrDefault(session => session.Id == id);
-
-            if (session == null) return NotFound();
-
-            _context.Remove(session);
-            _context.SaveChanges();
+            var result = _sessionService.DeleteSession(id);
+            
+            if (result.IsFailed) return NotFound();
 
             return NoContent();
         }
