b718011b6718cdafff0a09b77a0be5570d99bee0
Filipe Dias <filipediascontato@gmail.com>
2021-12-07 23:44:52 -0300

    Refacts the ManagerController

--- MoviesAPI/Controllers/ManagerController.cs
+++ MoviesAPI/Controllers/ManagerController.cs
@@ -1,8 +1,6 @@
-﻿using AutoMapper;
-using Microsoft.AspNetCore.Mvc;
-using MoviesAPI.Data;
+﻿using Microsoft.AspNetCore.Mvc;
 using MoviesAPI.Data.Dtos;
-using MoviesAPI.Models;
+using MoviesAPI.Services;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace MoviesAPI.Controllers
@@ -12,80 +10,69 @@ namespace MoviesAPI.Controllers
     [SwaggerTag("Create, read, update and delete managers")]
     public class ManagerController : ControllerBase
     {
-        private AppDbContext _context;
-        private IMapper _mapper;
+        private ManagerService _managerService;
         
-        public ManagerController(AppDbContext context, IMapper mapper)
+        public ManagerController(ManagerService managerService)
         {
-            _context = context;
-            _mapper = mapper;
+            _managerService = managerService;
         }
 
         [HttpPost(Name = "CreateManager")]
-        [SwaggerOperation(Summary = "Creates a new manager", Description = "Adds a new manager to the database")]
-        [SwaggerResponse(201, "The manager was created", typeof(Manager))]
-        [SwaggerResponse(400, "The manager data is invalid")]
+        [SwaggerOperation(Summary = "Creates a new manager.", Description = "Creates a new manager.")]
+        [SwaggerResponse(201, "The manager was created.", typeof(ReadManagerDto))]
+        [SwaggerResponse(400, "The manager data is invalid.")]
         public IActionResult CreateManager([FromBody] CreateManagerDto createManagerDto)
         {
-            var manager = _mapper.Map<Manager>(createManagerDto);
+            ReadManagerDto readManagerDto = _managerService.CreateManager(createManagerDto);
 
-            _context.Managers.Add(manager);
-            _context.SaveChanges();
-
-            return CreatedAtAction(nameof(GetManagerById), new { Id = manager.Id }, manager);
+            return CreatedAtAction(nameof(GetManagerById), new { Id = readManagerDto.Id }, readManagerDto);
         }
 
-        [HttpGet(Name = "GetAllManagers")]
-        [SwaggerOperation(Summary = "Lists all managers", Description = "Return all the managers in the database")]
-        [SwaggerResponse(200, "All existing managers have been listed", typeof(List<Manager>))]
-        public IActionResult GetAllManagers()
+        [HttpGet(Name = "GetManagers")]
+        [SwaggerOperation(Summary = "Gets a managers list.", Description = "Gets a managers list.")]
+        [SwaggerResponse(200, "All existing managers have been listed.", typeof(List<ReadManagerDto>))]
+        public IActionResult GetManagers()
         {
-            return Ok(_context.Managers);
+            var readManagerDto = _managerService.GetManagers();
+            
+            return Ok(readManagerDto);
         }
 
         [HttpGet("{id}", Name = "GetManagerById")]
-        [SwaggerOperation(Summary = "Lists a manager by id", Description = "Lists a manager by id")]
-        [SwaggerResponse(200, "The given manager has been listed", typeof(ReadManagerDto))]
-        [SwaggerResponse(404, "The given manager was not found")]
+        [SwaggerOperation(Summary = "Gets a manager by id.", Description = "Gets a manager by id.")]
+        [SwaggerResponse(200, "The given manager has been listed.", typeof(ReadManagerDto))]
+        [SwaggerResponse(404, "The given manager was not found.")]
         public IActionResult GetManagerById(int id)
         {
-            var manager = _context.Managers.FirstOrDefault(manager => manager.Id == id);
+            var readManagerDto = _managerService.GetManagerById(id);
 
-            if (manager == null) return NotFound();
-
-            var readManagerDto = _mapper.Map<ReadManagerDto>(manager);
+            if (readManagerDto == null) return NotFound();
 
             return Ok(readManagerDto);
         }
 
         [HttpPut("{id}", Name = "UpdateManager")]
-        [SwaggerOperation(Summary = "Updates a manager by id", Description = "Updates a manager by id")]
-        [SwaggerResponse(204, "The given manager has been updated")]
-        [SwaggerResponse(404, "The given manager was not found")]
+        [SwaggerOperation(Summary = "Updates a manager by id.", Description = "Updates a manager by id.")]
+        [SwaggerResponse(204, "The given manager has been updated.")]
+        [SwaggerResponse(404, "The given manager was not found.")]
         public IActionResult UpdateManger(int id, [FromBody] UpdateManagerDto updateManagerDto)
         {
-            var manager = _context.Managers.FirstOrDefault(manager => manager.Id == id);
+            var result = _managerService.UpdateManager(id, updateManagerDto);
 
-            if (manager == null) return NotFound();
-
-            _mapper.Map(updateManagerDto, manager);
-            _context.SaveChanges();
+            if (result.IsFailed) return NotFound();
 
             return NoContent();
         }
 
         [HttpDelete("{id}", Name = "DeleteManager")]
-        [SwaggerOperation(Summary = "Deletes a manager by id", Description = "Deletes a manager by id")]
-        [SwaggerResponse(204, "The given manager has been deleted")]
-        [SwaggerResponse(404, "The given manager was not found")]
+        [SwaggerOperation(Summary = "Deletes a manager by id.", Description = "Deletes a manager by id.")]
+        [SwaggerResponse(204, "The given manager has been deleted.")]
+        [SwaggerResponse(404, "The given manager was not found.")]
         public IActionResult DeleteManager(int id)
         {
-            var manager = _context.Managers.FirstOrDefault(manager => manager.Id == id);
+            var result = _managerService.DeleteManager(id);
 
-            if (manager == null) return NotFound();
-
-            _context.Remove(manager);
-            _context.SaveChanges();
+            if (result.IsFailed) return NotFound();
 
             return NoContent();
         }
