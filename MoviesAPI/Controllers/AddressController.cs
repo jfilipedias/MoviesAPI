f34c4c72be30109b6e12621659a8fd44cbed107e
Filipe Dias <filipediascontato@gmail.com>
2021-12-07 23:09:22 -0300

    Refacts the AddressController

--- MoviesAPI/Controllers/AddressController.cs
+++ MoviesAPI/Controllers/AddressController.cs
@@ -1,8 +1,6 @@
-﻿using AutoMapper;
-using Microsoft.AspNetCore.Mvc;
-using MoviesAPI.Data;
+﻿using Microsoft.AspNetCore.Mvc;
 using MoviesAPI.Data.Dtos;
-using MoviesAPI.Models;
+using MoviesAPI.Services;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace MoviesAPI.Controllers
@@ -12,81 +10,71 @@ namespace MoviesAPI.Controllers
     [SwaggerTag("Create, read, update and delete adressess")]
     public class AddressController : ControllerBase
     {
-        private AppDbContext _context;
-        private IMapper _mapper;
+        private AddressService _addressService;
 
-        public AddressController(AppDbContext context, IMapper mapper)
+        public AddressController(AddressService addressService)
         {
-            _context = context;
-            _mapper = mapper;
+            _addressService = addressService;
         }
 
         [HttpPost(Name = "CreateAddress")]
-        [SwaggerOperation(Summary = "Creates a new address", Description = "Adds a new address to the database")]
-        [SwaggerResponse(201, "The address was created", typeof(Address))]
-        [SwaggerResponse(400, "The address data is invalid")]
+        [SwaggerOperation(Summary = "Creates a new address.", Description = "Creates a new address.")]
+        [SwaggerResponse(201, "The address was created.", typeof(ReadAddressDto))]
+        [SwaggerResponse(400, "The address data is invalid.")]
         public IActionResult CreateAddress([FromBody] CreateAddressDto createAddressDto)
         {
-            var address = _mapper.Map<Address>(createAddressDto);
-
-            _context.Addresses.Add(address);
-            _context.SaveChanges();
-
-            return CreatedAtAction(nameof(GetAddressById), new { Id = address.Id }, address);
+            var readAddressDto = _addressService.CreateAddress(createAddressDto);
+            
+            return CreatedAtAction(nameof(GetAddressById), new { Id = readAddressDto.Id }, readAddressDto);
         }
 
-        [HttpGet(Name = "GetAllAddresses")]
-        [SwaggerOperation(Summary = "Lists all addresses", Description = "Return all the addresses in the database")]
-        [SwaggerResponse(200, "All existing addresses have been listed", typeof(List<Address>))]
-        public IActionResult GetAllAddresses()
+        [HttpGet(Name = "GetAddresses")]
+        [SwaggerOperation(Summary = "Gets all addresses.", Description = "Gets all addresses.")]
+        [SwaggerResponse(200, "All existing addresses have been listed.", typeof(List<ReadAddressDto>))]
+        public IActionResult GetAddresses()
         {
-            return Ok(_context.Addresses);
+            var readAddressDto = _addressService.GetAddresses();
+
+            return Ok(readAddressDto);
         }
 
         [HttpGet("{id}", Name = "GetAddressById")]
-        [SwaggerOperation(Summary = "Lists an address by id", Description = "Lists an address by id")]
-        [SwaggerResponse(200, "The given address has been listed", typeof(ReadAddressDto))]
-        [SwaggerResponse(404, "The given address was not found")]
+        [SwaggerOperation(Summary = "Gets an address by id.", Description = "Gets an address by id.")]
+        [SwaggerResponse(200, "The given address has been listed.", typeof(ReadAddressDto))]
+        [SwaggerResponse(404, "The given address was not found.")]
         public IActionResult GetAddressById(int id)
         {
-            var address = _context.Addresses.FirstOrDefault(address => address.Id == id);
+            var readAddressDto = _addressService.GetAddressById(id);
 
-            if (address == null) return NotFound();
+            if (readAddressDto == null) return NotFound();
 
-            var readAddressDto = _mapper.Map<ReadAddressDto>(address);
             return Ok(readAddressDto);
         }
 
         [HttpPut("{id}", Name = "UpdateAddress")]
-        [SwaggerOperation(Summary = "Updates an address by id", Description = "Updates an address by id")]
-        [SwaggerResponse(204, "The given address has been updated")]
-        [SwaggerResponse(404, "The given address was not found")]
+        [SwaggerOperation(Summary = "Updates an address by id.", Description = "Updates an address by id.")]
+        [SwaggerResponse(204, "The given address has been updated.")]
+        [SwaggerResponse(404, "The given address was not found.")]
         public IActionResult UpdateAddress(int id, [FromBody] UpdateAddressDto updateAddressDto)
         {
-            var address = _context.Addresses.FirstOrDefault(address => address.Id == id);
+            var result = _addressService.UpdateAddress(id, updateAddressDto);
+            
+            if (result.IsFailed) return NotFound();
 
-            if (address == null) return NotFound();
-
-            _mapper.Map(updateAddressDto, address);
-            _context.SaveChanges();
-
-            return Ok();
+            return NoContent();
         }
 
         [HttpDelete("{id}", Name = "DeleteAddress")]
-        [SwaggerOperation(Summary = "Deletes an address by id", Description = "Deletes an address by id")]
+        [SwaggerOperation(Summary = "Deletes an address by id.", Description = "Deletes an address by id.")]
         [SwaggerResponse(204, "The given address has been deleted")]
         [SwaggerResponse(404, "The given address was not found")]
         public IActionResult DeleteAddress(int id)
         {
-            var address = _context.Addresses.FirstOrDefault(address => address.Id == id);
+            var result = _addressService.DeleteAddress(id);
 
-            if (address == null) return NotFound();
+            if (result.IsFailed) return NotFound();
 
-            _context.Remove(address);
-            _context.SaveChanges();
-
-            return Ok();
+            return NoContent();
         }
     }
 }
