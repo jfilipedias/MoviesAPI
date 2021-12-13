c56c5f320f1c518e771fec734501b4c02e85861c
Filipe Dias <filipediascontato@gmail.com>
2021-12-12 23:43:44 -0300

    Creates the RegisterController

---
+++ UsersAPI/Controllers/RegisterController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsersAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        [HttpPost(Name = "RegisterUser")]
+        public IActionResult RegisterUser(CreateUserDto createUserDto)
+        {
+            // TODO: Calls the service
+
+            return Ok();
+        }
+    }
+}
