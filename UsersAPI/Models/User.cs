2431098565942e833829f935165a08adf81e1ee3
Filipe Dias <filipediascontato@gmail.com>
2021-12-12 23:44:02 -0300

    Creates the CreateUserDto and User model

---
+++ UsersAPI/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace UsersAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+    }
+}
