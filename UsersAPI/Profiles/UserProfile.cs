17d98307e46f16db46a13f1fc034fe70bd34dab0
Filipe Dias <filipediascontato@gmail.com>
2021-12-12 23:44:15 -0300

    Setups the automapper

---
+++ UsersAPI/Profiles/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using UsersAPI.Data.Dto;
+using UsersAPI.Models;
+
+namespace UsersAPI.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<CreateUserDto, User>();
+        }
+    }
+}
