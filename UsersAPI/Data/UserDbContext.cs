309dee98ee5a681a119fb36bbed5e45043017ee2
Filipe Dias <filipediascontato@gmail.com>
2021-12-15 00:08:17 -0300

    Creates the UserDbContext

---
+++ UsersAPI/Data/UserDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace UsersAPI.Data
+{
+    public class UserDbContext : IdentityDbContext<IdentityUser<int>, IdentityRole<int>, int>
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+        {
+        }
+    }
+}
