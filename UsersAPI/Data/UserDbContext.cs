7bceb6d3521fa1bf8fac0685f7df4054fd66e75e
Filipe Dias <filipediascontato@gmail.com>
2021-12-28 16:19:43 -0300

    Creates admin seed migration

--- UsersAPI/Data/UserDbContext.cs
+++ UsersAPI/Data/UserDbContext.cs
@@ -9,5 +9,31 @@ namespace UsersAPI.Data
         public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var adminUser = new IdentityUser<int>
+            {
+                UserName = "admin",
+                NormalizedUserName = "ADMIN",
+                Email = "admin@admin.com",
+                NormalizedEmail = "ADMIN@ADMIN.COM",
+                EmailConfirmed = true,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                Id = 9999
+            };
+
+            var passwordHasher = new PasswordHasher<IdentityUser<int>>();
+            adminUser.PasswordHash = passwordHasher.HashPassword(adminUser, "Admin123456!");
+
+            var adminRole = new IdentityRole<int> { Id = 9999, Name = "admin", NormalizedName = "ADMIN" };
+            var userRole = new IdentityUserRole<int> { RoleId = adminRole.Id, UserId = adminUser.Id };
+
+            builder.Entity<IdentityUser<int>>().HasData(adminUser);
+            builder.Entity<IdentityRole<int>>().HasData(adminRole);
+            builder.Entity<IdentityUserRole<int>>().HasData(userRole);
+        }
     }
 }
---
+++ UsersAPI/Migrations/20211228191013_Admin user.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UsersAPI.Migrations
+{
+    public partial class Adminuser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { 9999, "4fc4ad7f-dd23-4931-b6b5-ee3c12c623b8", "admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { 9999, 0, "1cd90433-f941-4a3e-bdce-b13ccd59fe9a", "admin@admin.com", true, false, null, "ADMIN@ADMIN.COM", "ADMIN", "AQAAAAEAACcQAAAAELFSh2YyismNC+NPPMiZnGSORNx83Gdp0Z13J5+XDDtYGCfiz5Co8GatZ0QdkChu4Q==", null, false, "3407d28e-7cea-482a-bd04-c557843ad248", false, "admin" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { 9999, 9999 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { 9999, 9999 });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 9999);
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 9999);
+        }
+    }
+}
